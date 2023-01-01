e9867e45ddca01277725c3e9d12f438216c9ef7e
NguyenTuanMinh76 <117766023+NguyenTuanMinh76@users.noreply.github.com>
2023-01-01 19:46:44 +0700

    Add files via upload

---
+++ #Bai 9.1.py
@@ -0,0 +1,10 @@
+def sign(x):
+    if x<0: x=-1
+    if x>0: x=1
+    if x==0: x=0
+    return str(x)
+<<<<<<< HEAD
+print(sign(0))
+=======
+print(sign(4))
+>>>>>>> 127802f4babb7d3f480dce06a86caf73d9e7eebc
---
+++ #Bai10.2.py
@@ -0,0 +1,5 @@
+kq=1
+def cacl(x):
+    kq=4*x-1
+    return kq
+cacl(7)
